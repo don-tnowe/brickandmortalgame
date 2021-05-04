b5561147bd7d718e775d206535440a30139ed43f
don-tnowe-port <gkrupenev@gmail.com>
2021-05-05 00:20:12 +0300

    Add Russian support and small adjustments (work's never done, is it?)

--- Scripts/HeroStates/HeroStateImmobile.cs
+++ Scripts/HeroStates/HeroStateImmobile.cs
@@ -1,14 +1,17 @@
-﻿using BrickAndMortal.Scripts.HeroComponents;
+using BrickAndMortal.Scripts.HeroComponents;
 
 namespace BrickAndMortal.Scripts.HeroStates
 {
-    class HeroStateImmobile : HeroState
-    {
-        public HeroStateImmobile(Hero hero) : base(hero) { }
+	class HeroStateImmobile : HeroState
+	{
+		public HeroStateImmobile(Hero hero) : base(hero) 
+		{ 
+			Hero.NodeTimerCoyote.Stop();
+		}
 
-        public override void MoveBody(float delta)
-        {
-            // Stop right there!!!
-        }
-    }
+		public override void MoveBody(float delta)
+		{
+			// Stop right there!!!
+		}
+	}
 }
